f45c9fa8361f03af1489c992989b4c9d465ac144
emanuelediluzio <emanuelediluzio0@gmail.com>
2023-07-28 23:34:45 +0200

    testing

--- frappe_whatsapp/utils/webhook.py
+++ frappe_whatsapp/utils/webhook.py
@@ -142,29 +142,6 @@ def send_notification_to_users(online_users, message):
         notification_message = f"Nuovo messaggio da {message['from']}: {message['text']['body']}"
         frappe.publish_realtime(event="notification", message=notification_message, user=user)
 
-"""Interagisci con l'AI e ottieni la risposta."""
-def get_ai_response(message):
-    api_key = "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"
-    endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
-    prompt = "Utente: {}\nAI:".format(message) + ",rispondi a tale domanda fingendo di essere un operatore della ASCOM Imola(puoi cercare informazioni su orari ecc sulla loro pagina), facendo pero attenzione a comunicare all'interlocutore di essere un intelligenza artificale e che appena un operatore sara' online ricevera' assistenza da quest'ultimo"
-
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + api_key,
-    }
-
-    data = {
-        "prompt": prompt,
-        "max_tokens": 150,
-    }
-
-    response = requests.post(endpoint, headers=headers, json=data)
-
-    if response.status_code == 200:
-        return response.json()["choices"][0]["text"]
-    else:
-        return "Si è verificato un errore nell'interazione con l'AI."
-    
 
 def update_message_status(data):
     """Update message status."""
