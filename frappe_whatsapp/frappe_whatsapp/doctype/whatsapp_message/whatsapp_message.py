4e0b7ca6c7a0d1d6ec69774ee10dc1153935322a
emanuelediluzio <emanuelediluzio00@gmail.com>
2023-08-12 10:10:20 +0200

    changes

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -34,7 +34,7 @@ class WhatsAppMessage(Document):
         if numero_utenti_online == 0 and self.type == 'Incoming': 
             data = {
                 "messaging_product": "whatsapp",
-                "to": self.format_number(self.get_mobile_number(self.from)),
+                "to": self.format_number(self.get_mobile_number(self['from'])),
                 "type": "text",
                 "preview_url": True,
                 "body": self.get_ai_response(self.message) ##ottengo la risposta dal messaggio in entrata dall'intelligenza artificiale
