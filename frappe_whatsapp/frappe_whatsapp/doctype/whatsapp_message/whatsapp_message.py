446008a911921b4b30ee9b3ba5f142fcc9a6dd0c
emanuelediluzio <emanuelediluzio00@gmail.com>
2023-08-12 15:21:48 +0200

    changes

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -6,20 +6,9 @@ import time
 import requests
 from frappe.model.document import Document
 from frappe.integrations.utils import make_post_request
-from active_users.utils.api import get_users
 
 
 class WhatsAppMessage(Document):
-
-    
-    """Send whats app messages."""
-    settings = frappe.get_doc(
-            "WhatsApp Settings", "WhatsApp Settings",
-        )
-    token = settings.get_password("token")
-
-    """Ricavo Token API OpenAi."""
-    token_api = settings.get_password("token_open_ai")
 
     def before_insert(self):
         """Send message."""
@@ -28,25 +17,6 @@ class WhatsAppMessage(Document):
                 link = frappe.utils.get_url() + '/' + self.attach
             else:
                 link = self.attach
-
-
-        online_users = get_users()
-        numero_utenti_online = len(online_users)
-        if numero_utenti_online == 0 and self.type == 'Incoming': 
-            data = {
-                "messaging_product": "whatsapp",
-                "to": self.format_number(self.get_mobile_number(self['from'])),
-                "type": "text",
-                "preview_url": True,
-                "body": self.get_ai_response(self.message) ##ottengo la risposta dal messaggio in entrata dall'intelligenza artificiale
-                    }
-
-            try:
-                self.notify(data)
-                self.status = "Success"
-            except Exception as e:
-                self.status = "Failed"
-            frappe.throw(f"Failed to send message: {str(e)}")
          
 
         if self.switch:
@@ -179,39 +149,6 @@ class WhatsAppMessage(Document):
 
         return number
     
-    def get_ai_response(self, message):
-     api_key = self.token_api
-     endpoint = "https://api.openai.com/v1/chat/completions"
-    
-     headers = {
-         "Content-Type": "application/json",
-         "Authorization": "Bearer " + api_key,
-     }
-
-     data = {
-        "messages": [
-            {"role": "system", "content": "Sei un intelligenza artificiale che impoersona un operatore di una chat di aiuto della ASCOM Imola, adesso ti fornisco varie info che puoi utilizzare per rispondere alle varie domande (IndirizzoAscom (Ufficio) a ImolaViale Rivalta 640026 ImolaDettagli di contatto 0542 619611* Fax.: 0542 619619 www.confcommercioimo... » Aggiungi il tuo indirizzo e-mail »Apre in 68:44 oreOrari di aperturaLunedi  08:30-12:00 e 14:30-16:30Martedi    08:30-12:00 e 14:30-16:30Mercoledi  08:30-12:00Giovedi  08:30-12:00 e 14:30-16:30Venerdi    08:30-12:00Sabato   chiusoDomenica  chiusoAdesso sono le ore 12:46), facendo pero attenzione a comunicare all'interlocutore di essere un intelligenza artificale e che appena un operatore sara' online ricevera' assistenza da quest'ultimo."},
-            {"role": "user", "content": message}
-        ],
-        "model": "gpt-3.5-turbo"  # Specifica il modello da utilizzare
-    }
-
-
-     response = requests.post(endpoint, headers=headers, json=data)
-
-     if response.status_code == 200:
-         choices = response.json()["choices"]
-         return choices[0]["message"]["content"]
-     else:
-       error_message = "Si è verificato un errore nell'interazione con l'AI."
-       if response.text:
-          try:
-             error_response = json.loads(response.text)
-             if "error" in error_response and "message" in error_response["error"]:
-                 error_message = error_response["error"]["message"]
-          except Exception as e:
-            pass
-     return error_message
     
     def get_mobile_number(self, customer_name):
      mobile_no = frappe.db.get_value("Customer", filters={"customer_name": customer_name}, fieldname="mobile_no")
